5521371b0f7f02821c6f3b4a6fbfe77a5cbc9861
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-21 01:55:34 -0500

    new user controller

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    console.log('createUserDto', createUserDto);
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<boolean> {
+    return this.userService.remove(id);
+  }
+}
