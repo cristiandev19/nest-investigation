310a87b02d42db231494d9a326a2022f1645aff1
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-21 01:55:26 -0500

    adding config module and typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './modules/user/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DB_HOST,
+      port: +process.env.PG_DB_PORT,
+      username: process.env.PG_DB_USERNAME,
+      password: process.env.PG_DB_PASSWORD,
+      database: process.env.PG_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [User],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
