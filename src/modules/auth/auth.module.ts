843dd4a82d24e7c0aded781c82496f6baa89c378
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-22 03:01:04 -0500

    creating auth module

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../user/user.module';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
