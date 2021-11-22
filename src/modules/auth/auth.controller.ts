05db6c03a70ea851e3f4bb7cbf2c034a7f2536cf
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-22 03:00:44 -0500

    new auth controller

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get()
+  findAll() {
+    return [];
+  }
+}
