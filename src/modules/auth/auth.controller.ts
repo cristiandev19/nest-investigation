c1b5276f100ece2d39107b5e96a6d68deb824e5d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-24 23:15:05 -0500

    adding guards into the auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,28 +1,42 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
+import { LoginDto } from './dtos/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
+  @Public()
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Body() login: LoginDto) {
+    console.log('req', login);
+    return this.authService.login({ userId: '1', username: 'dawdawdaw' });
   }
 
+  // if you want to use guard only in one method
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
+  getProfile(@Request() req): any {
     return req.user;
   }
 
   @Public()
   @Get()
   findAll() {
+    return [];
+  }
+
+  @Get('protected')
+  protectedFindAll() {
     return [];
   }
 }
